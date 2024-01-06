0135acec12be1d9640ceff706749578fc0c4d0f9
thabrkci <alpacinotaha@gmail.com>
2024-01-06 11:27:56 +0300

    trycatchsync

--- Program.cs
+++ Program.cs
@@ -1,50 +1,55 @@
-﻿using System.Reflection;
+﻿using System;
 
-namespace Hata_Yönetimi;
-
-class Program
+namespace Errormanagement
 {
-    static void Main(string[] args)
+    class Program
     {
-       //try//bu kısımda hata olup olmadığını alabilmek adına yazacağımız kod yazma bölümü(Kodu güvence altına almak için).
-       //{
-       //    Console.WriteLine("Bir sayi giriniz");
-       //int sayi = Convert.ToInt32(Console.ReadLine());
-       //    Console.WriteLine("Girmiş olduğunuz sayi:" +sayi);
-
-       //}
-       //catch(Exception ex)//bu kısımda hatayı yakaladığında ne yapılsın hangi mesaj verilsin kısmı?
-       //{
-       //    Console.WriteLine("Hata:" +ex.Message.ToString());
-
-       //}
-       //finally//bu kısımda hata alındıktan sonra ne yapılsın ?(zorunlu değil).
-       //{
-       //    Console.WriteLine("İşlem tamamland");
-       //
-       //}
-        
-        try
+        static void Main(string[] args)
         {
-           // int a = int.Parse("test");
-              int a = int.Parse("-2000000000");
-        }
-        catch (ArgumentNullException kex)
+            // Kullanıcıdan bir sayı girmesini isteyen ve girilen sayıyı ekrana yazdıran kod bloğu
+            // Ancak bu kısımda hata olup olmadığını kontrol etmek için try-catch-finally bloğu kullanılmamış.
 
-        {
-            Console.WriteLine("Boş değer girdiniz");
-            Console.WriteLine(kex);
-        }
-           // catch(FormatException kex)
-           // {
-           //        Console.WriteLine("Veri tipi uygun değil");
-           //        Console.WriteLine(kex);
-           // }
-           catch(OverflowException kex)
+            //try
+            //{
+            //    Console.WriteLine("Bir sayi giriniz");
+            //    int sayi = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine("Girmiş olduğunuz sayi:" + sayi);
+            //}
+            //catch(Exception ex)
+            //{
+            //    Console.WriteLine("Hata:" + ex.Message.ToString());
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("İşlem tamamlandı");
+            //}
+
+            // Hata yönetimi için try-catch bloğu kullanılmış.
+            try
             {
-                Console.WriteLine("Büyük yada küçük değer girdiniz");
-                Console.WriteLine(kex);
+                // Hata alması muhtemel bir kod parçası
+                // int a = int.Parse("test"); // Bu satır, FormatException'a neden olur.
+                int a = int.Parse("-2000000000");
             }
-
+            // ArgumentNullException, FormatException ve OverflowException türündeki hataların her biri için ayrı catch bloğu kullanılmış.
+            catch (ArgumentNullException kex)
+            {
+                // Boş değer girildiğinde bu blok çalışır.
+                Console.WriteLine("Boş değer girdiniz");
+                Console.WriteLine(kex); // Hatanın ayrıntılarını yazdırır.
+            }
+            catch (FormatException kex)
+            {
+                // Geçersiz bir formatta veri girildiğinde bu blok çalışır.
+                Console.WriteLine("Veri tipi uygun değil");
+                Console.WriteLine(kex); // Hatanın ayrıntılarını yazdırır.
+            }
+            catch (OverflowException kex)
+            {
+                // Büyük veya küçük bir değer girildiğinde bu blok çalışır.
+                Console.WriteLine("Büyük yada küçük değer girdiniz");
+                Console.WriteLine(kex); // Hatanın ayrıntılarını yazdırır.
+            }
+        }
     }
 }
---
+++ Readme.md
@@ -0,0 +1,7 @@
+Merhaba,
+Bu örneğimde sizlere hata olup olmadığını anlayabilmek adına,try cathch hakkında çok küçük bir örnek gösterdim.Umarım faydalı olur,açıklamalara yorum satırlarından ulaşabilirsiniz.
+Değerli vaktinizi ayırdığınız için teşekkür ederim.
+------------------------------------------------------
+Hello,
+In this example, I have shown you a very small example about try cathch in order to understand whether there is an error or not. I hope it will be useful, you can find explanations in the comments.
+Thank you for your valuable time.
